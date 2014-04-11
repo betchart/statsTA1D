b3e9ca25eecdefda3d88768d9917d5b4ca824062
Burton Betchart <betchart@gmail.com>
2014-04-11 01:20:08 -0400

    convert sysmag to 1d

--- finish/sysmag.py
+++ finish/sysmag.py
@@ -2,7 +2,6 @@
 
 import sys
 import math
-from lib.ellipse import ellipse
 import numpy as np
 from functools import partial
 import ROOT as r
@@ -20,49 +19,37 @@ class fitresult(object):
                    ]))
     labels = sum(pairs.values(),())
 
-    def __init__(self,partition):
-        self.summarize = summarize
-        self.file = r.TFile.Open('output/%s/asymmetry_%s_sys.root'%(partition,partition))
-        self.cfile = r.TFile.Open('output/%s/asymmetry_%s.root'%(partition,partition))
-        self.tfile = r.TFile.Open('output/%s/asymmetry_%s_t.root'%(partition,partition))
+    def __init__(self,var,partition):
+        name = "%s_%s"%(var,partition)
+        self.file = r.TFile.Open('output/%s/%s_sys.root'%(name,name))
+        self.cfile = r.TFile.Open('output/%s/%s.root'%(name,name))
+        self.tfile = r.TFile.Open('output/%s/%s_t.root'%(name,name))
         self.tree = self.file.Get('fitresult')
         self.ctree = self.cfile.Get('fitresult')
         self.ttree = self.tfile.Get('fitresult')
         self.ctree.GetEntry(0)
         self.extract()
 
-    @staticmethod
-    def sigmas(deltas): return np.outer(*(2*[deltas]))
-    def deltas(self,e): return (e.fitX - self.ctree.fitX, e.fitY - self.ctree.fitY)
-
-    @staticmethod
-    def sigmaAC(sigmas2):
-        R = np.array([[1,1],[-1,1]]) # rotate pi/4, except also scale by sqrt(2)
-        return math.sqrt(R.dot(sigmas2.dot(R.T))[0,0])
+    def delta(self,e): return e.fit - self.ctree.fit
 
     @property
     def sigma_mcStat(self):
-        return self.sigmaAC( sum([self.sigmas(self.deltas(e)) for e in self.ttree],self.sigmas([0,0])) / self.ttree.GetEntries() )
+        return math.sqrt( sum([self.delta(e)**2 for e in self.ttree]) / self.ttree.GetEntries() )
 
     def extract(self):
         self.values = {}
-        pdftot = {'deltas':(0,0),'dA':0}
         for e in self.tree:
             label = max(os.path.commonprefix([e.label,l]) for l in self.labels)
             if label not in self.labels: continue
-            self.values[label] = {'deltas':self.deltas(e),
-                                  'dA':sum(self.deltas(e)),
-                                  'd':math.sqrt(np.dot(*(2*[self.deltas(e)])))
-                              }
+            self.values[label] = self.delta(e)
 
-        self.order = sorted(self.values, key = lambda k: self.values[k]['d'], reverse=True)
-        self.pvalues = dict([(name, math.sqrt(0.5*(self.values[a]['dA']**2 + self.values[b]['dA']**2))) for name,(a,b) in self.pairs.items() if a])
+        self.order = sorted(self.values, key = lambda k: self.values[k], reverse=True)
+        self.pvalues = dict([(name, math.sqrt(0.5*(self.values[a]**2 + self.values[b]**2))) for name,(a,b) in self.pairs.items() if a])
         self.pvalues['MC stat.'] = self.sigma_mcStat
         self.pvalues['PDF'] = math.sqrt(sum(x**2 for L,x in self.pvalues.items() if 'PD' in L))
         for key in list(self.pvalues):
             if 'PD:' in key: del self.pvalues[key]
         self.porder = sorted(self.pvalues, key = lambda k: self.pvalues[k], reverse=True)
-        self.pvalues2 = dict([(name, self.sigmaAC((0.5*sum([np.outer(*(2*[self.values[q]['deltas']])) for q in [a,b]],self.sigmas([0,0]))))) for name,(a,b) in self.pairs.items() if a])
 
         self.pvalues['Total'] = math.sqrt(sum(x**2 for x in self.pvalues.values()))
         self.porder.append('Total')
@@ -70,19 +57,20 @@ class fitresult(object):
     def form(self,key):
         top = key in self.order[:5]+self.porder[:5]
         form = (r"\textbf{% .3f}" if top else "% .3f ").rjust(20)
-        d = (self.values[key]['d'] if key in self.order else self.pvalues[key])*100
+        d = (self.values[key] if key in self.order else self.pvalues[key])*100
         return (form%d).ljust(20)
-        
+
 
 if __name__ == '__main__':
 
-    summarize=True
+    summarize = True
+
     partitions = [('full','Full Selection'),
                   ('loM',r'$m_{\ttbar}<450\GeV$'),
                   ('hiM',r'$m_{\ttbar}>450\GeV$'),
                   ('loY',r'$\tanh\abs{y_{\ttbar}}<0.5$'),
-                  ('hiY',r'$\tanh\abs{y_{\ttbar}}>0.5$')]
-    results = [fitresult(p) for p,_ in partitions]
+                  ('hiY',r'$\tanh\abs{y_{\ttbar}}>0.5$')][:1]
+    results = [fitresult('XL',p) for p,_ in partitions]
 
     def formkey(key) :
         subs = [('PD_',r'pdf'),
