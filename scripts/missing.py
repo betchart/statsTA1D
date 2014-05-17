6c3da3bd0b8a3a0f2a207236bb9ad926dca2c3c0
Burton Betchart <betchart@gmail.com>
2014-05-16 20:46:55 -0400

    show total

--- scripts/missing.py
+++ scripts/missing.py
@@ -6,4 +6,4 @@ import lib
 pat = sys.argv[1]
 
 nums = [int(f.replace(pat,'').replace('.root','')) for f in lib.getCommandOutput("ls %s*.root"%pat)["stdout"].split()]
-print ','.join([str(s) for s in sorted(set(range(max(nums))) - set(nums))])
+print max(nums), ': ', ','.join([str(s) for s in sorted(set(range(max(nums))) - set(nums))])
