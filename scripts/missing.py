01032712c4313d7d945cfb0af70c6e97bfe04151
Burton Betchart <betchart@gmail.com>
2014-09-02 14:53:02 -0400

    allow for non-empty suffix pattern in scripts/missing

--- scripts/missing.py
+++ scripts/missing.py
@@ -4,6 +4,7 @@ import sys
 import lib
 
 pat = sys.argv[1]
+end = '' if len(sys.argv)<3 else sys.argv[2]
 
-nums = [int(f.replace(pat,'').replace('.root','')) for f in lib.getCommandOutput("ls %s*.root"%pat)["stdout"].split()]
+nums = [int(f.replace(pat,'').replace(end+'.root','')) for f in lib.getCommandOutput("ls %s*.root"%pat)["stdout"].split()]
 print max(nums), ': ', ','.join([str(s) for s in sorted(set(range(max(nums))) - set(nums))])
