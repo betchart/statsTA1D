7e58194b675cfb200affc86a7576ae4f43536573
Burton Betchart <betchart@gmail.com>
2014-12-18 00:11:43 -0500

    Errorbars on calibrations from sample statistics rather than from expected lumi

--- finish/bias_plot2.py
+++ finish/bias_plot2.py
@@ -3,10 +3,12 @@ import numpy as np
 import lib
 from lib.__autoBook__ import autoBook
 from itertools import izip
-import ROOT as r
 import math
-
 import matplotlib
+
+import ROOT as r
+r.gROOT.SetBatch(1)
+
 class bias_plot(object):
     def __init__(self, trees, treesA, treesC):
         import matplotlib.pyplot as plt
@@ -16,7 +18,8 @@ class bias_plot(object):
         lw = 1.3
         fig = plt.figure(figsize=(6.5,6.5))
         ax = fig.add_subplot(111)
-        ax.set_ylim(-0.4,0.4)
+        ylimabs = 0.8
+        ax.set_ylim(-ylimabs,ylimabs)
         ax.set_xlim(-2,2)
         ax.set_ylabel(r'Measurement Bias $(\%)$', fontsize=fs)
         ax.set_xlabel(r'$A_c^y (\%)$ : Calculated', fontsize=fs)
@@ -46,7 +49,7 @@ class bias_plot(object):
             fit,sigma = lib.combined_result([(e.fit,e.sigma),(m.fit,m.sigma)])
             label = e.label[4:6]
             bookC.fill( e.gen_Ac*100, 'gen_'+label, 1000, -2, 2)
-            bookC.fill( fit*100, "mean_"+label, 100, (e.gen_Ac-e.scale)*100, (e.gen_Ac+e.scale)*100)
+            bookC.fill( fit*100, "mean_"+label, 100, (e.gen_Ac-2*e.scale)*100, (e.gen_Ac+2*e.scale)*100)
 
         cgen = []
         cfit = []
@@ -55,11 +58,19 @@ class bias_plot(object):
         iLow = []
         iHi = []
         iOther = []
+        fitfile = "output/bias_plot2-fits.pdf"
+        canvas = r.TCanvas()
+        canvas.Print(fitfile+'[')
         for i, (k,v) in enumerate(x for x in bookC.items() if 'gen' not in x[0]):
             if 'gen' in k: continue
             cgen.append(bookC[k.replace('mean','gen')].GetMean())
-            cfit.append(v.GetMean())
-            cerr.append(v.GetMeanError())
+            #cfit.append(v.GetMean())
+            #cerr.append(v.GetMeanError())
+            v.Scale(1./v.Integral())
+            v.Fit("gaus","QEML")
+            canvas.Print(fitfile)
+            cfit.append(v.GetFunction("gaus").GetParameter('Mean'))
+            cerr.append(v.GetFunction("gaus").GetParameter('Sigma'))
             clab.append(k)
             print k
             if '.' in k:
@@ -67,6 +78,7 @@ class bias_plot(object):
             elif '2' in k:
                 iHi.append(i)
             else: iOther.append(i)
+        canvas.Print(fitfile+']')
 
         #ax.errorbar(cgen,cfit-np.array(cgen),yerr=cerr,fmt='.', color=(0,0,0.85), mec='k', label=r'Alternative $\mathrm{t\bar{t}}$ models')
         ax.errorbar(np.array(cgen)[iOther],(cfit-np.array(cgen))[iOther],yerr=np.array(cerr)[iOther],fmt='.', color=(0,0,0.85), mec='k')
--- start/measurement.py
+++ start/measurement.py
@@ -7,6 +7,7 @@ from ensembles import calibration_specs
 from fitroutine import fit
 from asymmNames import genNames
 import os
+import copy
 import inputs
 
 class measurement(object):
@@ -107,6 +108,76 @@ class measurement(object):
         sampleList = [c['sample'] for c in calibration_specs() if c['which']==which]
         prePre = pars['dirIncrement'] in [0,4,5]
 
+        # get Ac_phi_ttalt and Ac_y_ttalt
+        filePattern = 'data/stats_top_mu_%s.root'
+        tag = 'ph_sn_jn_20'
+        tfile = r.TFile.Open(filePattern%tag)
+        h = lib.get(tfile,'genTopTanhDeltaAbsY_genTopDPtDPhi/'+ sampleList[0])
+        Ac_y_ttalt = lib.asymmetry(h.ProjectionX())[0]
+        Ac_phi_ttalt = lib.asymmetry(h.ProjectionY())[0]
+        tfile.Close()
+
+        args = {
+            'signal':pars['signal'],
+            'sigPrefix':pars['sigPre'],
+            'dirPrefix':"R%02d" % (pars['R0_'] + pars['dirIncrement']),
+            'genDirPre':pars['genDirPre'],
+            'prePre':prePre,
+            'templateID':None,
+            'sampleList': sampleList,
+            'rebin':pars['rebin'],
+            'no3D':pars['no3D']
+            }
+
+        for i in range(*calSlice):
+            args.update({'templateID':i})
+            alt_channels = dict( [ ((lep,part), inputs.channel_data(lep, part, **args))
+                                   for lep in ['el','mu'] for part in ['top','QCD']] )
+
+            if 'xsfactor' in kwargs:
+                alt_channels[('el','top')].samples['ttalt'].xs *= kwargs['xsfactor']
+                alt_channels[('mu','top')].samples['ttalt'].xs *= kwargs['xsfactor']
+
+    
+            model = copy.deepcopy(self.central.model)
+            model.import_alt_model(alt_channels, pars['nobg'])
+            wGen = model.w
+    
+            # bring xs_ttalt to the most consistant value possible
+            wGen.arg('d_xs_ttalt').setVal((wGen.arg('expect_mu_tt').getVal() + wGen.arg('expect_el_tt').getVal()) /
+                                          (wGen.arg('expect_mu_ttalt').getVal() + wGen.arg('expect_el_ttalt').getVal()) - 1)
+            if not (-0.5 < wGen.arg('d_xs_ttalt').getVal() < 1.5):
+                print 'ttalt xs invalid! Adjust calibration_specs!'
+                exit()
+                # not clear how to do the same for factor_*_qcd (equivalent bg representations)
+    
+            truth = {'Ac': Ac_y_ttalt if genNames['XL'][3:] in pars['signal'] else Ac_phi_ttalt}
+            altItems = ['expect_%s_ttalt'%s for s in ['el','mu']]
+            for item in (set(fit.modelItems()+altItems)-set()): truth[item] = wGen.arg(item).getVal()
+            truth.update({'Ac_raw_el_model':model.Ac_raw('el','alt%smodel'%pars['nobg']),
+                          'Ac_raw_mu_model':model.Ac_raw('mu','alt%smodel'%pars['nobg'])})
+    
+
+            alt = wGen.pdf('alt%smodel'%pars['nobg']).generateBinned( wGen.argSet(','.join(model.observables+['channel'])),
+                                                                      0.,
+                                                                      r.RooFit.ExpectedData(True) )
+
+            pars['label'] = '%s_cal%s%03d'%(label,which,i)
+            pars['quiet'] = True
+            with open(self.outNameBase + pars['label'] + '.log', 'w') as log:
+                pars['log']=log
+                f = fit(altData=alt, **pars)
+            f.ttreeWrite(self.outNameBase + pars['label'] + '.root', truth)
+            if self.doVis: f.model.visualize(self.outNameBase + pars['label'] + '.pdf', nobg=pars['nobg'])
+
+
+
+    @roo.quiet
+    def old_calibrations(self, pars, which='mn', calSlice=(None,None), N=1000, label='', **kwargs):
+
+        sampleList = [c['sample'] for c in calibration_specs() if c['which']==which]
+        prePre = pars['dirIncrement'] in [0,4,5]
+
         args = {
             'signal':pars['signal'],
             'sigPrefix':pars['sigPre'],
@@ -158,7 +229,7 @@ class measurement(object):
                                r.RooFit.Extended(True)
                            )
         mcstudy.generate(N,0,True)
-        for i in range(N)[slice(*calSlice)]:
+        for i in range(*calSlice):
             alt = mcstudy.genData(i)
             pars['label'] = '%s_cal%s%03d'%(label,which,i)
             pars['quiet'] = True
